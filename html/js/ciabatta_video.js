fa148b96cdf873ee62924060b99ecf3a6324e030
Simone S <xsimone97@hotmail.it>
2019-01-19 17:34:13 +0100

    Category selection button fix

--- html/js/ciabatta_video.js
+++ html/js/ciabatta_video.js
@@ -360,6 +360,18 @@ function getCookie(c_name) {
 
 //END OF COOKIE SECTION
 
+//deativate all highlighted buttons
+function changeButtonPopular(){
+	document.getElementById("pills-random-tab").classList.remove("active");
+	document.getElementById("pills-search-tab").classList.remove("active");
+	document.getElementById("pills-related-tab").classList.remove("active");
+	document.getElementById("pills-recent-tab").classList.remove("active");
+	document.getElementById("pills-fvitali-tab").classList.remove("active");
+	document.getElementById("pills-similar-tab").classList.remove("active");
+	document.getElementById("pills-search-tab").cclassList.remove("active");
+	//document.getElementById("dropdownMenuLink").classList.add("active");
+}
+
 //VIDEO-SUGGESTION CATEGORIES
 
 function changeSuggestedVideos(index){
@@ -386,14 +398,26 @@ function changeSuggestedVideos(index){
 			current_category = 4;
 			getRecentlyWatchedVideos(uc_value);
 			break;
-		case 5:
+			case 5:
 			current_category = 5;
 			console.log("cat 5");
 			break;
-		case 52:
-		  current_category = 52;
-		  carica_video_popolarita_relativa();
-		  break;
+		case 50://Global absolute
+			changeButtonPopular();
+			document.getElementById("pills-popular-tab").classList.add("active");
+			current_category = 50;
+			break;
+		case 51://Local absolute
+			changeButtonPopular();
+			document.getElementById("pills-popular-tab-absloc").classList.add("active");
+			current_category = 51;
+			break;
+		case 52://Local relative
+		    changeButtonPopular();
+		    document.getElementById("pills-popular-tab-relloc").classList.add("active");
+		    current_category = 52;
+		    carica_video_popolarita_relativa();
+		    break;
 		case 6:
 			current_category = 6;
 			console.log("cat 6");
