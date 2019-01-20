4fd17fef45a353486bedfc4de556943cd0471bd9
Gabri <mfgabri52@gmail.com>
2019-01-20 11:24:29 +0100

    Update modal.js
    inserita pushstate

--- html/js/modal.js
+++ html/js/modal.js
@@ -9,6 +9,16 @@ $('document').ready(function(){
 		});
 		$('.modal-body').html(videoInfo);
 	 	$('.modal-body a').on("click", function(){
+			//History Browser Manipulation
+			var video2insertInHistory = {};
+			video2insertInHistory.videoId = this.id;
+			video2insertInHistory.image = this.image;
+			video2insertInHistory.reason = this.reason;
+			video2insertInHistory.title = this.title;
+			history.pushState(video2insertInHistory, "", "#"+this.id);
+			console.log("video nuovo: \n " + JSON.stringify(video2insertInHistory));
+			console.log("video vecchio: \n " + JSON.stringify(video_info));
+			//fine History Browser Manipulation
 			closeModalShowVideo(this.id);
 		});
 	});
